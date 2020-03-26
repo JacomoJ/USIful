c8d499ded33300682056fa9e109739d76ed5b000
rebigor <rebigor@gmail.com>
2020-03-26 22:20:52 +0100

    Add files via upload

--- app/src/main/java/ch/usi/digitalprototype/PriorityList.java
+++ app/src/main/java/ch/usi/digitalprototype/PriorityList.java
@@ -10,5 +10,6 @@ public class PriorityList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_priority_list);
+        // add code about drag and drop
     }
 }
