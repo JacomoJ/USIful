0f6aa2cd98e3256649bab66544da8b16aaefd7a2
rebigor <rebigor@gmail.com>
2020-03-26 22:02:42 +0100

    Add files via upload

---
+++ app/src/main/java/ch/usi/digitalprototype/ActTimeTracking.java
@@ -0,0 +1,14 @@
+package ch.usi.digitalprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActTimeTracking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_time_tracking);
+    }
+}
---
+++ app/src/main/java/ch/usi/digitalprototype/PriorityList.java
@@ -0,0 +1,14 @@
+package ch.usi.digitalprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PriorityList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_priority_list);
+    }
+}
